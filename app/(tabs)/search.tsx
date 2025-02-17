bde5f518f51caeaafa3122c77bfdd47c46a90892
Bowen Liu <bowenliu22@gmail.com>
2025-02-16 17:25:20 -0800

    created search page, updated styles file

---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,11 @@
+import colors from '@/src/styles/themes/colors';
+import { Text, View, StyleSheet } from 'react-native';
+import { styles } from '@/src/styles/globalStyles';
+
+export default function SearchScreen() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Search screen</Text>
+      </View>
+    );
+}
